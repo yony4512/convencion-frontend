0cbc4bd99bb0a1c1a124beb5ccd07f8c5079619f
yack <ychillitupa.231ds10@gmail.com>
2025-07-19 23:14:25 -0500

    feat: Actualiza los enlaces de redes sociales en el footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,4 +1,4 @@
-import { Facebook, Instagram, Twitter, MapPin, Phone, Mail, Book } from 'lucide-react';
+import { Facebook, Instagram, MapPin, Phone, Mail, Book } from 'lucide-react';
 import Logo from '../ui/Logo';
 import { StyledLink as Link } from '../ui/Link';
 
@@ -14,14 +14,26 @@ const Footer = () => {
               La mejor pollería de Cusco, ofreciendo el auténtico sabor peruano desde 2010. Nuestro pollo a la brasa está preparado con ingredientes frescos y la receta tradicional.
             </p>
             <div className="mt-6 flex space-x-4">
-              <Link href="https://facebook.com" className="text-gray-300 hover:text-white transition-colors">
+              <Link href="https://www.facebook.com/share/v/1G8wnrBf2G/" target="_blank" className="text-gray-300 hover:text-white transition-colors">
                 <Facebook size={20} />
               </Link>
-              <Link href="https://instagram.com" className="text-gray-300 hover:text-white transition-colors">
+              <Link href="https://www.instagram.com/reel/DMUEcSQsuF7/?igsh=MTg4Mmxkd3l2amF6NA==" target="_blank" className="text-gray-300 hover:text-white transition-colors">
                 <Instagram size={20} />
               </Link>
-              <Link href="https://twitter.com" className="text-gray-300 hover:text-white transition-colors">
-                <Twitter size={20} />
+              <Link href="https://vm.tiktok.com/ZMSnFRNbu/" target="_blank" className="text-gray-300 hover:text-white transition-colors">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path>
+                </svg>
               </Link>
             </div>
           </div>
